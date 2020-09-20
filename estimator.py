c06a0621c2659d3aa2fc62567c9da38fefa6e9ce
nithiiyan <nskha101@uottawa.ca>
2020-09-19 23:07:11 -0400

    first commit

---
+++ estimator.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+
+data = pd.read_csv("student-mat.csv", sep=";")
+print("data attributes before trim")
+print(data.head())
+
+print("data attributes after trim")
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+print(data.head())
+
+predict = "G3"
+print("data label" + predict)
+
+X = np.array(data.drop([predict], 1)) 
+y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+accuracy = linear.score(x_test, y_test) # acc stands for accuracy 
+
+print(accuracy)
+
+print('Coefficient: \n', linear.coef_) # These are each slope value
+print('Intercept: \n', linear.intercept_) # This is the intercept
+
+predictions = linear.predict(x_test) # Gets a list of all predictions
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
